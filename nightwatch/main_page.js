a43f361b8c48c0719874ef8081b77098157633a7
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-12 14:16:54 +0100

    Fixed eslint problem

--- nightwatch/main_page.js
+++ nightwatch/main_page.js
@@ -2,7 +2,7 @@ module.exports = {
 	'Main page': function ( client ) {
 		client
 		.url( 'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page' )
-		.assert.elementPresent( 'li#pt-login a')
+		.assert.elementPresent( 'li#pt-login a' )
 		.end();
 	}
 };
