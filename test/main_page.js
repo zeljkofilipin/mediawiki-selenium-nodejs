e79b56fc9eb6e35105ea6e60edbcbebca8c906c1
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-14 18:21:18 +0100

    replaced main page between test and webdriverjs folders

--- test/main_page.js
+++ test/main_page.js
@@ -1,25 +1,16 @@
+// setup
 var assert = require( 'assert' ),
 	webdriver = require( 'selenium-webdriver' ),
 	By = webdriver.By,
-	test = require( 'selenium-webdriver/testing' );
+	driver = new webdriver.Builder()
+	.forBrowser( 'chrome' )
+	.build();
 
-test.describe( 'Main page', function () {
-	var driver;
-
-	test.beforeEach( function () {
-		driver = new webdriver.Builder()
-		.forBrowser( 'chrome' )
-		.build();
-	} );
-
-	test.afterEach( function () {
-		driver.quit();
-	} );
-
-	test.it( 'should have "Log in" link', function () {
-		driver.get( 'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page' );
-		driver.findElement( By.linkText( 'Log in' ) ).isDisplayed().then( function ( displayed ) {
-			assert( displayed );
-		} );
-	} );
+// test
+driver.get( 'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page' );
+driver.findElement( By.linkText( 'Log in' ) ).isDisplayed().then( displayed => {
+	assert( displayed );
 } );
+
+// teardown
+driver.quit();
--- webdriverjs/main_page.js
+++ webdriverjs/main_page.js
@@ -1,16 +1,25 @@
-// setup
 var assert = require( 'assert' ),
 	webdriver = require( 'selenium-webdriver' ),
 	By = webdriver.By,
-	driver = new webdriver.Builder()
-	.forBrowser( 'chrome' )
-	.build();
+	test = require( 'selenium-webdriver/testing' );
 
-// test
-driver.get( 'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page' );
-driver.findElement( By.linkText( 'Log in' ) ).isDisplayed().then( displayed => {
-	assert( displayed );
+test.describe( 'Main page', function () {
+	var driver;
+
+	test.beforeEach( function () {
+		driver = new webdriver.Builder()
+		.forBrowser( 'chrome' )
+		.build();
+	} );
+
+	test.afterEach( function () {
+		driver.quit();
+	} );
+
+	test.it( 'should have "Log in" link', function () {
+		driver.get( 'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page' );
+		driver.findElement( By.linkText( 'Log in' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
 } );
-
-// teardown
-driver.quit();
